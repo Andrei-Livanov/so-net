0ae6838bacdb9013969864e10e02f14486b1436e
Andrei <livanovandrej99@gmail.com>
2021-07-13 13:46:32 +0700

    Add color and font for sonet

--- src/main/resources/js/pages/App.vue
+++ src/main/resources/js/pages/App.vue
@@ -1,96 +1,100 @@
 <template>
-    <v-app>
-        <v-app-bar app>
-            <v-toolbar-title>Sonet</v-toolbar-title>
-            <v-btn
-                    text
-                    v-if="profile"
-                    :disabled="$route.path === '/'"
-                    @click="showMessages"
-            >
-                Messages
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn
-                    text
-                    v-if="profile"
-                    :disabled="$route.path === '/user'"
-                    @click="showProfile"
-            >
-                {{profile.name}}
-            </v-btn>
-            <v-btn v-if="profile" icon href="/logout">
-                <v-icon>{{ exitToAppIcon }}</v-icon>
-            </v-btn>
-        </v-app-bar>
-        <v-content>
-            <router-view></router-view>
-        </v-content>
-    </v-app>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="t">Sonet</v-toolbar-title>
+      <v-btn
+          text
+          v-if="profile"
+          :disabled="$route.path === '/'"
+          @click="showMessages"
+      >
+        Messages
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn
+          text
+          v-if="profile"
+          :disabled="$route.path === '/user'"
+          @click="showProfile"
+      >
+        {{ profile.name }}
+      </v-btn>
+      <v-btn v-if="profile" icon href="/logout">
+        <v-icon>{{ exitToAppIcon }}</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-    import { mapState, mapMutations } from 'vuex';
-    import { mdiExitToApp } from '@mdi/js'
-    import { addHandler } from 'util/ws';
+import {mapState, mapMutations} from 'vuex';
+import {mdiExitToApp} from '@mdi/js'
+import {addHandler} from 'util/ws';
 
-    export default {
-        computed: mapState(['profile']),
-        methods: {
-            ...mapMutations([
-                'addMessageMutation',
-                'updateMessageMutation',
-                'removeMessageMutation',
-                'addCommentMutation'
-            ]),
-            showMessages() {
-                this.$router.push('/');
-            },
-            showProfile() {
-                this.$router.push('/user');
-            }
-        },
-        data() {
-            return {
-                exitToAppIcon: mdiExitToApp
-            }
-        },
-        created() {
-            addHandler(data => {
-                if (data.objectType === 'MESSAGE') {
-                    switch(data.eventType) {
-                            case 'CREATE':
-                                this.addMessageMutation(data.body);
-                                break;
-                            case 'UPDATE':
-                                this.updateMessageMutation(data.body);
-                                break;
-                            case 'REMOVE':
-                                this.removeMessageMutation(data.body);
-                                break;
-                            default:
-                                console.error(`Looks like the event type is unknown "${data.eventType}".`);
-                    }
-                } else if (data.objectType === 'COMMENT') {
-                    switch(data.eventType) {
-                            case 'CREATE':
-                                this.addCommentMutation(data.body);
-                                break;
-                            default:
-                                console.error(`Looks like the event type is unknown "${data.eventType}".`);
-                    }
-                } else {
-                    console.error(`Looks like the object type is unknown "${data.objectType}".`);
-                }
-            })
-        },
-        beforeMount() {
-            if (!this.profile) {
-                this.$router.replace('/auth')
-            }
-        }
+export default {
+  computed: mapState(['profile']),
+  methods: {
+    ...mapMutations([
+      'addMessageMutation',
+      'updateMessageMutation',
+      'removeMessageMutation',
+      'addCommentMutation'
+    ]),
+    showMessages() {
+      this.$router.push('/');
+    },
+    showProfile() {
+      this.$router.push('/user');
     }
+  },
+  data() {
+    return {
+      exitToAppIcon: mdiExitToApp
+    }
+  },
+  created() {
+    addHandler(data => {
+      if (data.objectType === 'MESSAGE') {
+        switch (data.eventType) {
+          case 'CREATE':
+            this.addMessageMutation(data.body);
+            break;
+          case 'UPDATE':
+            this.updateMessageMutation(data.body);
+            break;
+          case 'REMOVE':
+            this.removeMessageMutation(data.body);
+            break;
+          default:
+            console.error(`Looks like the event type is unknown "${data.eventType}".`);
+        }
+      } else if (data.objectType === 'COMMENT') {
+        switch (data.eventType) {
+          case 'CREATE':
+            this.addCommentMutation(data.body);
+            break;
+          default:
+            console.error(`Looks like the event type is unknown "${data.eventType}".`);
+        }
+      } else {
+        console.error(`Looks like the object type is unknown "${data.objectType}".`);
+      }
+    })
+  },
+  beforeMount() {
+    if (!this.profile) {
+      this.$router.replace('/auth')
+    }
+  }
+}
 </script>
 
 <style>
+.t {
+  color: green;
+  font-family: 'Roboto', sans-serif;
+}
 </style>
